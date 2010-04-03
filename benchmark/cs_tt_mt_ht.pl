616dcbad9c4268246064835e7472b78f9bd49a1c
gfx <gfuji@cpan.org>
2010-04-03 19:14:57 +0900

    Add MST to cs_tt_mt_ht.pl

--- benchmark/cs_tt_mt_ht.pl
+++ benchmark/cs_tt_mt_ht.pl
@@ -1,24 +1,41 @@
-#!perl -w
+#!perl
+use strict;
+use warnings;
 use Text::ClearSilver;
 use Template;
 use Text::MicroTemplate 'build_mt';
 use HTML::Template::Pro;
 use Benchmark ':all';
 
-my $cs = Text::ClearSilver->new();
+my $cs = Text::ClearSilver->new(VarEscapeMode => 'html');
 
-my $tt_tmpl = q{ [% foo  %] } x 10;
-my $cs_tmpl = q{ <?cs var:foo ?> } x 10;
+my $tt_tmpl  = q{ [% foo  %] }            x 10;
+my $mst_tmpl = q{ $= h:foo $ }            x 10;
+my $cs_tmpl  = q{ <?cs var:foo ?> }       x 10;
+my $mt_tmpl  = q{ <?=  $_[0]->{foo} ?> }  x 10;
+my $ht_tmpl  = q{ <tmpl_var name="foo"> } x 10;
 
-my $mt = build_mt(q{ <?=  $_[0] ?> } x 10);
+my $mt = build_mt($mt_tmpl);
 my $ht = HTML::Template::Pro->new(
-    scalarref => \(q{ <tmpl_var name="foo"> } x 10),
+    scalarref => \$ht_tmpl,
 );
 my $tt = Template->new();
 
+my $has_mst = eval { require MobaSiF::Template };
+
+my %vars = (foo => 'bar');
+
 printf "%vd %s\n", $^V, $^O;
-foreach my $mod(qw(Template Text::MicroTemplate HTML::Template::Pro Text::ClearSilver)) {
+foreach my $mod(qw(Template Text::MicroTemplate
+    HTML::Template::Pro Text::ClearSilver),
+    $has_mst ? 'MobaSiF::Template' : ()) {
     print $mod, ": ", $mod->VERSION, "\n";
+}
+
+my $mst_bin = 'mst.bin';
+if($has_mst) {
+    MobaSiF::Template::Compiler::compile(\$mst_tmpl, $mst_bin);
+    eval q{ END{ unlink $mst_bin } };
 }
 
 if(0){
@@ -26,32 +43,34 @@ if(0){
     warn _tt();
     warn _mt();
     warn _ht();
+    warn _mst();
     exit;
 }
 
 cmpthese( -1, => {
-        'cs' => \&_cs,
-        'tt' => \&_tt,
-        'mt' => \&_mt,
-        'ht' => \&_ht,
+        'CS'  => \&_cs,
+        'TT'  => \&_tt,
+        'MT'  => \&_mt,
+        'HT'  => \&_ht,
+        $has_mst ? ('MST' => \&_mst) : (),
     },
 );
 
 sub _cs {
-    $cs->process(\$cs_tmpl, { foo => 'bar' }, \my $out );
+    $cs->process(\$cs_tmpl, \%vars, \my $out );
     $out;
 }
-
 sub _tt {
-    $tt->process(\$tt_tmpl, {foo => 'bar'}, \my $out) or die;
+    $tt->process(\$tt_tmpl, \%vars, \my $out) or die;
     $out;
 }
-
 sub _mt {
-    $mt->('bar');
+    $mt->(\%vars);
 }
-
 sub _ht {
-    $ht->param(foo => 'bar');
+    $ht->param(%vars);
     my $out = $ht->output();
+}
+sub _mst {
+    MobaSiF::Template::insert($mst_bin, \%vars);
 }
